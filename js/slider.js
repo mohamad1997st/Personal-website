9ddd69f443e3d13e1b6466fd5e70e3fa7b520127
hsoubecommorce <hsoubecommerce@gamil.com>
2021-05-18 14:38:15 +0300

    first commit

---
+++ js/slider.js
@@ -0,0 +1,16 @@
+var $links = $(".itemLinks");
+
+$links.click(function (e) {
+    $links.removeClass("active");
+    var clickedLink = e.target;
+    clickedLink = $(clickedLink);
+    var position = clickedLink.attr("data-pos");
+    var translateValue = "translate3d("+ position * 25 +"%, 0px, 0)"
+    $("#wrapper").css({
+        transform: translateValue
+    });
+
+    clickedLink.addClass("active");
+});
+
+$($links[0]).addClass("active");
