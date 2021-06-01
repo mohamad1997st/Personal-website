96b678cfc6d669d0797b7745714e3ca575b6933e
hsoubecommorce <hsoubecommerce@gamil.com>
2021-06-01 22:29:12 +0300

    second commit

--- js/slider.js
+++ js/slider.js
@@ -5,7 +5,7 @@ $links.click(function (e) {
     var clickedLink = e.target;
     clickedLink = $(clickedLink);
     var position = clickedLink.attr("data-pos");
-    var translateValue = "translate3d("+ position * 25 +"%, 0px, 0)"
+    var translateValue = "translateX("+ position * 25 +"%)"
     $("#wrapper").css({
         transform: translateValue
     });
